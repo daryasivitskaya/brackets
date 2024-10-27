116cfbf8f10205929f483fae1da5ba5090089f31
Darya Sivitskaya <dara.sivitskaya@gmail.com>
2024-10-27 20:04:23 +0100

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+const OPEN_BRACKETS = [];
+const BRACKETS_PAIR = {};
+
+for (const [open, close] of bracketsConfig) {
+  OPEN_BRACKETS.push(open);
+  BRACKETS_PAIR[close] = open;
 }
+
+function isBracketsOk(str) {
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      if (BRACKETS_PAIR[currentSymbol] === currentSymbol) {
+        if (stack[stack.length - 1] === currentSymbol) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      const topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+return isBracketsOk(str);
+};
